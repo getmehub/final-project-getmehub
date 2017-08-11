121afbeb4354f455e6bc81edde9a4e39325947f2
getmehub <maharjan@mail.sfsu.edu>
2017-08-11 16:00:43 -0700

    witty level .05

---
+++ app/src/main/java/com/mm/witty/Viewpage/cartoons.java
@@ -0,0 +1,22 @@
+package com.mm.witty.Viewpage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mm.witty.R;
+
+
+/**
+ * Created by MacBook on 8/11/17.
+ */
+
+public class cartoons extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xkcd_witty);
+
+    }
+}
